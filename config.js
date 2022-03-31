00d46d2b540f76da7410a30b58b733a9c798f365
Sidak1311 <67144572+Sidak1311@users.noreply.github.com>
2022-03-31 14:12:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDxssyuDF30y3ce2uLyaadbAv_WYeJNouA",
+    authDomain: "story-telling-app-93ce9.firebaseapp.com",
+    projectId: "story-telling-app-93ce9",
+    storageBucket: "story-telling-app-93ce9.appspot.com",
+    messagingSenderId: "64344096050",
+    appId: "1:64344096050:web:995d0bbc93f1fe295dab2b"
+};
